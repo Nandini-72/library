8003045e5fd2ac029b6e16c5a99da8e8e1835973
Nandini <jainnandini741@gmail.com>
2020-09-08 17:59:05 +0530

    DataBase error

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.DATABASE_URL,{
     useNewUrlParser: true 
 })
 const db = mongoose.connection
-db.on('error',()=>console.error(error))
+db.on('error',()=>console.error('error'))
 db.once('open',()=>console.log('connected to mongoose'))
 
 app.use('/',indexRouter)
